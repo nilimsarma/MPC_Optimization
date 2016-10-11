4e6842797dd14222b1d694abaa5e5c39a72c6327
Nilim Sarma <nilim.ch.sarma@gmail.com>
2016-10-10 20:10:13 -0400

    ..

--- mpc_formulate.hpp
+++ mpc_formulate.hpp
@@ -5,7 +5,7 @@
 
 double mpc_objective (const double State_variables[NUM_STATE_VARIABLES], const double Control_variables[NUM_CONTROL_VARIABLES]);
 double mpc_objective_end_term(const double State_variables[NUM_STATE_VARIABLES], const double Control_variables[NUM_CONTROL_VARIABLES]);
-void mpc_initial_value (double[NUM_STATE_VARIABLES] State_variables_initial_value);
+void mpc_initial_value (double State_variables_initial_value[NUM_STATE_VARIABLES]);
 void mpc_state_differential (const double State_variables[NUM_STATE_VARIABLES], const double Control_variables[NUM_CONTROL_VARIABLES], double dot_State_variables [NUM_STATE_VARIABLES]);
 void mpc_path_constraints (const double State_variables[NUM_STATE_VARIABLES], const double Control_variables[NUM_CONTROL_VARIABLES], double Constraints [NUM_PATH_CONSTRAINTS]);
 double mpc_terminal_constraints(const double State_variables[NUM_STATE_VARIABLES]);
